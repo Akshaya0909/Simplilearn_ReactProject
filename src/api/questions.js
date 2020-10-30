ad9eb923e3c4dea0a1b1e61d7f29195e9254a11d
Akshaya <varshaya0909@gmail.com>
2020-10-30 19:27:58 +0530

    Added React Quiz project

---
+++ src/api/questions.js
@@ -0,0 +1,134 @@
+var quizQuestions = [
+    {
+      question: "What author would you rather read about ornithology from?",
+      answerindex : 1,
+      answers: [
+        {
+          type: "IBM",
+          content: "Salim Ali",
+          answer : false
+        },
+        {
+          type: "amdocs",
+          content: "Stephen King",
+          answer : false
+        },
+        {
+          type: "Sony2",
+          content: "J K Rowling",
+          answer : false
+        },
+        {
+          type: "Sony1",
+          content: "Dan Brown",
+          answer : false
+        }
+      ]
+    },
+    {
+      question: "Which place would you prefer to play with friends?",
+      answerindex : 1,
+      answers: [
+        {
+          type: "IBM",
+          content: "Park",
+          answer : false
+        },
+        {
+          type: "amdocs",
+          content: "Coffee Shop",
+          answer : false
+        },
+        {
+          type: "Sony",
+          content: "Mall",
+          answer : false
+        },
+        {
+          type: "Sonyq",
+          content: "Radio Station",
+          answer : false
+        }
+      ]
+    },
+    {
+      question: "Which of these racing franchises would you prefer to play a game from?",
+      answerindex : 1,
+      answers: [
+        {
+          type: "IBM",
+          content: "Forza",
+          answer : false
+        },
+        {
+          type: "amdocs",
+          content: "Mario Kart",
+          answer : false
+        },
+        {
+          type: "Sony",
+          content: "Gran Turismo",
+          answer : false
+        },
+        {
+          type: "Sonyw",
+          content: "Playstation 1qq",
+          answer : false
+        }
+      ]
+    },
+    {
+      question: "Which of these games do you think is best?",
+      answerindex : 1,
+      answers: [
+        {
+          type: "IBM",
+          content: "BioShock",
+          answer : false
+        },
+        {
+          type: "amdocs",
+          content: "The Legend of Zelda: Ocarina of Time",
+          answer : false
+        },
+        {
+          type: "Sony",
+          content: "Final Fantasy VII",
+          answer : false
+        },
+        {
+          type: "Sonyr",
+          content: "Playstation 1",
+          answer : false
+        }
+      ]
+    },
+    {
+      question: "Who is the author the brief history of time?",
+      answerindex : 1,
+      answers: [
+        {
+          type: "IBM",
+          content: "Stephen Hawking",
+          answer : false
+        },
+        {
+          type: "amdocs",
+          content: "Elif Shafak",
+          answer : false
+        },
+        {
+          type: "Sony",
+          content: "Murakami",
+          answer : false
+        },
+        {
+          type: "Sonyqqå",
+          content: "Carl Sagan",
+          answer : false
+        }
+      ]
+    }
+  ];
+  
+  export default quizQuestions;
---
+++ src/components/Result.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Result extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  
+  renderQuestins(){
+    return  this.props.quizResult.map((_data,index) =>{
+        return <div className="list-grp">{_data.question} <br/>correct answer is : {_data.answerindex} :  and you have selected {this.props.answers[index]+1} { ((this.props.answers[index]+1) === _data.answerindex) ? (
+            <span className="status">Correct Answer!</span>) : (
+                <span className="wstatus">Hard Luck!</span>)}</div>
+    })
+  }
+  render (){
+    return (
+    <div  className="quiz-story">
+      <div>
+        <strong>Lets see your results</strong>!
+        <div>{this.renderQuestins()}</div>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default Result;
